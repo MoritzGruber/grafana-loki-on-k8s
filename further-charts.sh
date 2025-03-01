20f181a613b0b087942fe43ed1b345332cedca2a
Moritz Gruber <m.gruber@cyclus.digital>
2025-03-01 15:59:27 +0100

    chore: adjust config a bit
    chore: adjust config a bit

--- further-charts.sh
+++ further-charts.sh
@@ -5,3 +5,6 @@ helm install kube-state-metrics prometheus-community/kube-state-metrics -n monit
 # cAdvisor
 helm repo add ckotzbauer https://ckotzbauer.github.io/helm-charts
 helm install cadvisor ckotzbauer/cadvisor -n monitoring
+
+kubectl annotate service -n monitoring cadvisor prometheus.io/scrape=true prometheus.io/port=8080
+kubectl annotate service -n monitoring kube-state-metrics prometheus.io/scrape=true prometheus.io/port=8080
