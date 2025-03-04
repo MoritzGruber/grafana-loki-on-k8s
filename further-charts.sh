742994c017ec133761497c1ac02de1d366b2025c
Moritz Gruber <m.gruber@cyclus.digital>
2025-03-04 10:27:46 +0100

    chore: update chardts
    chore: update chardts

--- further-charts.sh
+++ further-charts.sh
@@ -1,10 +1,10 @@
 
 # kube-state-metrics
-helm install kube-state-metrics prometheus-community/kube-state-metrics -n monitoring
+helm upgrade --install kube-state-metrics prometheus-community/kube-state-metrics -n monitoring
 
 # cAdvisor
 helm repo add ckotzbauer https://ckotzbauer.github.io/helm-charts
-helm install cadvisor ckotzbauer/cadvisor -n monitoring
+helm upgrade --install cadvisor ckotzbauer/cadvisor -n monitoring
 
 kubectl annotate service -n monitoring cadvisor prometheus.io/scrape=true prometheus.io/port=8080
 kubectl annotate service -n monitoring kube-state-metrics prometheus.io/scrape=true prometheus.io/port=8080
